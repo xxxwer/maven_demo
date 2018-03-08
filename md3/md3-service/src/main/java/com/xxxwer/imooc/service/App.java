ad719ea9ca940d96ab2ad8bab8764a4e07b9b87f
xxxwer <wertrust@126.com>
2018-03-08 17:28:54 +0800

    mvn archetype:generate == to create this demo

---
+++ md3/md3-service/src/main/java/com/xxxwer/imooc/service/App.java
@@ -0,0 +1,13 @@
+package com.xxxwer.imooc.service;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
